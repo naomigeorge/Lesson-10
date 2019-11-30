adb951fec0827632ff171417fe5201bec510356a
naomigeorge <35682117+naomigeorge@users.noreply.github.com>
2019-11-29 16:48:13 -0800

    Update weatherapi.js

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e238cca28558d6bb8d70b2efa85398d0&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e238cca28558d6bb8d70b2efa85398d0&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
